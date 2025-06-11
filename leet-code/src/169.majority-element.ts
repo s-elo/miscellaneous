5885cf7ee70a812d21efefa368ead5da5df14506
s-elo <zz321yy@qq.com>
2025-06-11 21:44:39 +0800

    feat: leet-code Remove Duplicates from Sorted Array II

---
+++ leet-code/src/169.majority-element.ts
@@ -0,0 +1,11 @@
+/*
+ * @lc app=leetcode id=169 lang=typescript
+ *
+ * [169] Majority Element
+ */
+
+// @lc code=start
+function majorityElement(nums: number[]): number {
+  return 0;
+}
+// @lc code=end
--- leet-code/src/80.remove-duplicates-from-sorted-array-ii.ts
+++ leet-code/src/80.remove-duplicates-from-sorted-array-ii.ts
@@ -4,8 +4,25 @@
  * [80] Remove Duplicates from Sorted Array II
  */
 
+removeDuplicates([0, 0, 1, 1, 1, 1, 2, 3, 3]);
+
 // @lc code=start
 function removeDuplicates(nums: number[]): number {
-  return 0;
+  let k = 1;
+  let count = 0;
+
+  for (let i = 1; i < nums.length; i++) {
+    if (nums[i] === nums[i - 1]) {
+      count++;
+      if (count < 2) {
+        nums[k++] = nums[i];
+      }
+    } else {
+      count = 0;
+      nums[k++] = nums[i];
+    }
+  }
+
+  return k;
 }
 // @lc code=end
