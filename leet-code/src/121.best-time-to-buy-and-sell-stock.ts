e53e2b8297a40f107cfd93b41c4bf7aa01f7d58c
s-elo <zz321yy@qq.com>
2025-06-14 19:00:00 +0800

    feat(leet-code): best-time-to-buy-and-sell-stock

--- leet-code/src/121.best-time-to-buy-and-sell-stock.ts
+++ leet-code/src/121.best-time-to-buy-and-sell-stock.ts
@@ -4,8 +4,22 @@
  * [121] Best Time to Buy and Sell Stock
  */
 
+maxProfit([7, 1, 5, 3, 6, 4]); // Output: 5
+
 // @lc code=start
-function maxProfit(prices: number[]): number {
-  return 0;
+export function maxProfit(prices: number[]): number {
+  let profit = 0;
+
+  let buyPrice = Infinity;
+  for (const price of prices) {
+    if (price < buyPrice) {
+      buyPrice = price; // Update the minimum price to buy
+    } else {
+      const currentProfit = price - buyPrice; // Calculate profit if sold at current price
+      profit = Math.max(profit, currentProfit); // Update maximum profit if current profit is higher
+    }
+  }
+
+  return profit;
 }
 // @lc code=end
---
+++ leet-code/src/122.best-time-to-buy-and-sell-stock-ii.ts
@@ -0,0 +1,11 @@
+/*
+ * @lc app=leetcode id=122 lang=typescript
+ *
+ * [122] Best Time to Buy and Sell Stock II
+ */
+
+// @lc code=start
+function maxProfit(prices: number[]): number {
+  return 0;
+}
+// @lc code=end
