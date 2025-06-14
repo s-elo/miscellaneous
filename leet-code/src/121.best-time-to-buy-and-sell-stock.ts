f7e0a6d392ac64dd838015baa6579d40c84178cc
s-elo <zz321yy@qq.com>
2025-06-14 18:44:29 +0800

    feat(leet-code): rotate-array

---
+++ leet-code/src/121.best-time-to-buy-and-sell-stock.ts
@@ -0,0 +1,11 @@
+/*
+ * @lc app=leetcode id=121 lang=typescript
+ *
+ * [121] Best Time to Buy and Sell Stock
+ */
+
+// @lc code=start
+function maxProfit(prices: number[]): number {
+  return 0;
+}
+// @lc code=end
--- leet-code/src/189.rotate-array.ts
+++ leet-code/src/189.rotate-array.ts
@@ -4,11 +4,48 @@
  * [189] Rotate Array
  */
 
+rotate([1, 2, 3, 4, 5, 6, 7], 3);
+
 // @lc code=start
 /**
  Do not return anything, modify nums in-place instead.
  */
 function rotate(nums: number[], k: number): void {
-  return;
+  // return solution1(nums, k);
+  return solution2(nums, k);
+}
+
+/** SPACE: O(1), TIME: O(2n) */
+export function solution2(nums: number[], k: number) {
+  const reverse = (nums: number[], start: number, end: number) => {
+    while (start < end) {
+      const temp = nums[start];
+      nums[start] = nums[end];
+      nums[end] = temp;
+      start++;
+      end--;
+    }
+  };
+
+  // k might exceeds nums.length, making re-rotating
+  k = k % nums.length;
+
+  reverse(nums, 0, nums.length - 1);
+  reverse(nums, 0, k - 1);
+  reverse(nums, k, nums.length - 1);
+}
+
+/** SPACE: O(1), TIME: O(2kn) */
+export function solution1(nums: number[], k: number) {
+  while (k) {
+    // here takes O(n) time
+    const last = nums.pop();
+    if (last === undefined) {
+      return;
+    }
+    // here takes another O(n) time
+    nums.unshift(last);
+    k--;
+  }
 }
 // @lc code=end
