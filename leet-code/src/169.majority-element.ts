331b9cad81488230dab0f0ef6d2d030b4130cbb5
s-elo <zz321yy@qq.com>
2025-06-14 12:39:12 +0800

    feat(leet-code): majority-element

--- leet-code/src/169.majority-element.ts
+++ leet-code/src/169.majority-element.ts
@@ -4,8 +4,57 @@
  * [169] Majority Element
  */
 
+majorityElement([3, 2, 3]);
+
 // @lc code=start
 function majorityElement(nums: number[]): number {
-  return 0;
+  // return solution1(nums);
+  // return solution2(nums);
+  return solution3(nums);
+}
+
+/**
+ * Space: O(1), Time: O(n),
+ * Boyer-Moore Voting Algorithm
+ */
+export function solution3(nums: number[]) {
+  let cadidate;
+  let count = 0;
+
+  for (const num of nums) {
+    if (count === 0) {
+      cadidate = num;
+    }
+
+    count += cadidate === num ? 1 : -1;
+  }
+
+  return cadidate;
+}
+
+/** Space: O(1), Time: O(n*log n),
+ * utilize the info that majority element is more than n/2
+ * */
+export function solution2(nums: number[]) {
+  return nums.sort((a, b) => a - b)[Math.floor(nums.length / 2)];
+}
+
+export function solution1(nums: number[]) {
+  const countMap = new Map<number, number>();
+
+  for (const num of nums) {
+    const count = countMap.get(num) ?? 0;
+    countMap.set(num, count + 1);
+  }
+
+  return [...countMap.entries()].reduce(
+    (max, [num, count]) => {
+      if (count > max.count) {
+        return { count, num };
+      }
+      return max;
+    },
+    { count: 0, num: nums[0] },
+  ).num;
 }
 // @lc code=end
---
+++ leet-code/src/189.rotate-array.ts
@@ -0,0 +1,14 @@
+/*
+ * @lc app=leetcode id=189 lang=typescript
+ *
+ * [189] Rotate Array
+ */
+
+// @lc code=start
+/**
+ Do not return anything, modify nums in-place instead.
+ */
+function rotate(nums: number[], k: number): void {
+  return;
+}
+// @lc code=end
