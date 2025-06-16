36f354e5e27dc789db94cb97ab1abc25c4972cc9
s-elo <zz321yy@qq.com>
2025-06-16 20:14:12 +0800

    feat(leet-code): insert-delete-get-random-o1

---
+++ leet-code/src/238.product-of-array-except-self.ts
@@ -0,0 +1,11 @@
+/*
+ * @lc app=leetcode id=238 lang=typescript
+ *
+ * [238] Product of Array Except Self
+ */
+
+// @lc code=start
+function productExceptSelf(nums: number[]): number[] {
+  return [];
+}
+// @lc code=end
--- leet-code/src/380.insert-delete-get-random-o-1.ts
+++ leet-code/src/380.insert-delete-get-random-o-1.ts
@@ -6,20 +6,35 @@
 
 // @lc code=start
 class RandomizedSet {
-  constructor() {
-    return;
-  }
+  protected _store: number[] = [];
+  protected _map = new Map<number, number>();
 
   insert(val: number): boolean {
+    const idx = this._map.get(val);
+    if (idx !== undefined) return false;
+
+    this._store.push(val);
+    this._map.set(val, this._store.length - 1);
+
     return true;
   }
 
   remove(val: number): boolean {
+    const idx = this._map.get(val);
+    if (idx === undefined) return false;
+
+    const lastVal = this._store[this._store.length - 1];
+    this._store[idx] = lastVal;
+    this._map.set(lastVal, idx);
+    this._store.pop();
+    this._map.delete(val);
+
     return true;
   }
 
   getRandom(): number {
-    return 0;
+    const idx = Math.floor(Math.random() * this._store.length);
+    return this._store[idx];
   }
 }
 
@@ -30,4 +45,10 @@ class RandomizedSet {
  * var param_2 = obj.remove(val)
  * var param_3 = obj.getRandom()
  */
+
+const obj = new RandomizedSet();
+const param1 = obj.insert(1);
+const param2 = obj.remove(2);
+const param3 = obj.getRandom();
+console.log(param1, param2, param3);
 // @lc code=end
