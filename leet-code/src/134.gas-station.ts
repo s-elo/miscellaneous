24609b481fb79a322684562fe53a0d0b40d09024
s-elo <zz321yy@qq.com>
2025-06-17 20:32:43 +0800

    feat(leet-code): product-of-array-except-self

---
+++ leet-code/src/134.gas-station.ts
@@ -0,0 +1,11 @@
+/*
+ * @lc app=leetcode id=134 lang=typescript
+ *
+ * [134] Gas Station
+ */
+
+// @lc code=start
+function canCompleteCircuit(gas: number[], cost: number[]): number {
+  return 0;
+}
+// @lc code=end
--- leet-code/src/238.product-of-array-except-self.ts
+++ leet-code/src/238.product-of-array-except-self.ts
@@ -4,8 +4,58 @@
  * [238] Product of Array Except Self
  */
 
+productExceptSelf([1, 2, 3, 4]);
+
 // @lc code=start
 function productExceptSelf(nums: number[]): number[] {
-  return [];
+  // return solution1(nums);
+  return solution2(nums);
+}
+
+/** O(2n) and no need to handle 0 cases */
+export function solution2(nums: number[]) {
+  const ret: number[] = [];
+
+  // calculate all the left product of corresponding element
+  // and store the val in the position
+  let left = 1;
+  nums.forEach((num, i) => {
+    ret[i] = left;
+    left *= num;
+  });
+
+  // calculate all the right product of corresponding element
+  // multiply with the value in the position, namely the previous all the left product
+  let right = 1;
+  for (let i = nums.length - 1; i >= 0; i--) {
+    ret[i] *= right;
+    right *= nums[i];
+  }
+
+  return ret;
+}
+
+/** O(2n) */
+export function solution1(nums: number[]) {
+  let zeroCount = 0;
+  const totalProduct = nums.reduce((total, num) => {
+    if (num === 0) zeroCount++;
+    return total * (num === 0 ? 1 : num);
+  }, 1);
+
+  if (zeroCount > 1) return new Array(nums.length).fill(0);
+
+  const ret: number[] = [];
+  for (const num of nums) {
+    if (num === 0) {
+      ret.push(totalProduct);
+    } else if (zeroCount > 0) {
+      ret.push(0);
+    } else {
+      ret.push(totalProduct / num);
+    }
+  }
+
+  return ret;
 }
 // @lc code=end
