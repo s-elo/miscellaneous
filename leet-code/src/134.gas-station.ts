125a7206f667eb189693f19fdfc18be3b0f3c0d1
s-elo <zz321yy@qq.com>
2025-06-21 10:11:19 +0800

    feat(leet-code): gas-station

--- leet-code/src/134.gas-station.ts
+++ leet-code/src/134.gas-station.ts
@@ -4,8 +4,37 @@
  * [134] Gas Station
  */
 
+canCompleteCircuit([1, 2, 3, 4, 5], [3, 4, 5, 1, 2]);
+
 // @lc code=start
+/**
+ * @thoughts
+ * - if the total gas is less than the total cost, there is no way to complete the circuit
+ * - start from the i station, keep tracking the current gas,
+ * if at j station, the current gas is not enough to reach the next station,
+ * then it means we cannot start from any station between i and j,
+ * so we can set the next start station to j + 1
+ */
 function canCompleteCircuit(gas: number[], cost: number[]): number {
-  return 0;
+  let start = 0;
+  let totalGas = 0;
+  let totalCost = 0;
+  let curGas = 0;
+
+  for (let i = 0; i < gas.length; i++) {
+    const gasAtStation = gas[i];
+    const costToNext = cost[i];
+
+    if (curGas < 0) {
+      start = i;
+      curGas = 0;
+    }
+
+    curGas += gasAtStation - costToNext;
+    totalGas += gasAtStation;
+    totalCost += costToNext;
+  }
+
+  return totalGas < totalCost ? -1 : start;
 }
 // @lc code=end
---
+++ leet-code/src/135.candy.ts
@@ -0,0 +1,11 @@
+/*
+ * @lc app=leetcode id=135 lang=typescript
+ *
+ * [135] Candy
+ */
+
+// @lc code=start
+function candy(ratings: number[]): number {
+  return 0;
+}
+// @lc code=end
