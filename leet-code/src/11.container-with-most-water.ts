a0198ab91cb5a130c9a508c6ec3616910e4d878b
s-elo <zz321yy@qq.com>
2025-07-20 19:27:23 +0800

    feat(leet-code): two sum with sorted array

---
+++ leet-code/src/11.container-with-most-water.ts
@@ -0,0 +1,11 @@
+/*
+ * @lc app=leetcode id=11 lang=typescript
+ *
+ * [11] Container With Most Water
+ */
+
+// @lc code=start
+function maxArea(height: number[]): number {
+  return 0;
+}
+// @lc code=end
--- leet-code/src/167.two-sum-ii-input-array-is-sorted.ts
+++ leet-code/src/167.two-sum-ii-input-array-is-sorted.ts
@@ -4,8 +4,26 @@
  * [167] Two Sum II - Input Array Is Sorted
  */
 
+twoSum([2, 7, 11, 15], 9);
+
 // @lc code=start
 function twoSum(numbers: number[], target: number): number[] {
+  let left = 0;
+  let right = numbers.length - 1;
+
+  while (left < right) {
+    const sum = numbers[left] + numbers[right];
+    if (sum === target) {
+      return [left + 1, right + 1];
+    }
+
+    if (sum < target) {
+      left++;
+    } else {
+      right--;
+    }
+  }
+
   return [];
 }
 // @lc code=end
