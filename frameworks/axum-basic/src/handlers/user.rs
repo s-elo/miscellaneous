c29ee8ec3e14a1b683d02c0c76e240a797a924fe
s-elo <zz321yy@qq.com>
2025-10-22 15:50:54 +0800

    feat: add request_id

--- frameworks/axum-basic/src/handlers/user.rs
+++ frameworks/axum-basic/src/handlers/user.rs
@@ -18,6 +18,7 @@ pub async fn create_user_handler(
   // matches our application
   AppJson(params): AppJson<UserParams>,
 ) -> Result<ApiRes<User>, AppError> {
+  tracing::info!("create_user_handler: {}", params.name);
   let user = state.services.user_service.create_user(params.name)?;
 
   Ok(ApiRes::success(user))
@@ -26,6 +27,7 @@ pub async fn create_user_handler(
 pub async fn get_all_users_handler(
   State(state): State<AppState>,
 ) -> Result<ApiRes<Vec<User>>, AppError> {
+  tracing::info!("get_all_users_handler");
   let users = state.services.user_service.get_all_users().await?;
 
   Ok(ApiRes::success(users))
--- frameworks/axum-basic/src/routes/root.rs
+++ frameworks/axum-basic/src/routes/root.rs
@@ -1,21 +1,40 @@
 use axum::{
   Router,
   extract::{MatchedPath, Request},
+  http::HeaderName,
   middleware::from_fn,
   routing::IntoMakeService,
 };
-use tower_http::trace::TraceLayer;
 
-use crate::{
-  middlewares::logs::log_app_errors,
-  routes::user::user_routes,
-  state::app::{AppState, Services},
+use tower::ServiceBuilder;
+use tower_http::{
+  request_id::{MakeRequestUuid, PropagateRequestIdLayer, SetRequestIdLayer},
+  trace::TraceLayer,
 };
 
+use crate::{middlewares::logs::log_app_errors, routes::user::user_routes, state::app::AppState};
+
+const REQUEST_ID_HEADER: &str = "x-request-id";
+
 pub fn init_routes() -> IntoMakeService<Router> {
-  let app = Router::new()
-    // A dummy route that accepts some JSON but sometimes fails
-    .merge(user_routes().with_state(AppState::default()))
+  let x_request_id = HeaderName::from_static(REQUEST_ID_HEADER);
+
+  let middleware = ServiceBuilder::new()
+    .layer(SetRequestIdLayer::new(
+      x_request_id.clone(),
+      MakeRequestUuid,
+    ))
+    .layer(TraceLayer::new_for_http().make_span_with(|req: &Request| {
+      let request_id = req.headers().get(REQUEST_ID_HEADER);
+
+      match request_id {
+        Some(req_id) => tracing::info_span!("req_id", request_id = ?req_id),
+        None => {
+          tracing::error!("could not extract request_id");
+          tracing::info_span!("req_id")
+        }
+      }
+    }))
     .layer(
       TraceLayer::new_for_http()
         // Create our own span for the request and include the matched path. The matched
@@ -30,13 +49,20 @@ pub fn init_routes() -> IntoMakeService<Router> {
             .get::<MatchedPath>()
             .map(|matched_path| matched_path.as_str());
 
-          tracing::debug_span!("request", %method, %uri, matched_path)
+          tracing::debug_span!("|", %method, %uri, matched_path)
         })
         // By default `TraceLayer` will log 5xx responses but we're doing our specific
         // logging of errors so disable that
         .on_failure(()),
     )
+    // send headers from request to response headers
+    .layer(PropagateRequestIdLayer::new(x_request_id))
     .layer(from_fn(log_app_errors));
+
+  let app = Router::new()
+    // A dummy route that accepts some JSON but sometimes fails
+    .merge(user_routes().with_state(AppState::default()))
+    .layer(middleware);
 
   app.into_make_service()
 }
