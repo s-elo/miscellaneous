36e37be79a32a9717b4887bfb8f8fbe6a81e05e9
s-elo <zz321yy@qq.com>
2025-10-26 16:57:21 +0800

    feat(leet-code): container-with-most-water

--- leet-code/src/11.container-with-most-water.ts
+++ leet-code/src/11.container-with-most-water.ts
@@ -4,8 +4,27 @@
  * [11] Container With Most Water
  */
 
+maxArea([1, 8, 6, 2, 5, 4, 8, 3, 7]);
+
 // @lc code=start
 function maxArea(height: number[]): number {
-  return 0;
+  let left = 0;
+  let right = height.length - 1;
+  let maxArea = 0;
+
+  while (left < right) {
+    const width = right - left;
+    const curHeight = Math.min(height[left], height[right]);
+    const area = width * curHeight;
+    maxArea = Math.max(maxArea, area);
+
+    if (height[left] < height[right]) {
+      left++;
+    } else {
+      right--;
+    }
+  }
+
+  return maxArea;
 }
 // @lc code=end
